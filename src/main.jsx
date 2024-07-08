af66ff6e9e550f6a7b0ffc925c4d25f1338d8a6c
Camille CRAPAT <camille.capt@gmail.com>
2024-07-08 19:23:28 +0200

    Initilisation du projet

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import './global.css';
+
+import Header from './components/Header';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import User from './pages/User';
+import Error from './pages/Error';
+import Footer from './components/Footer';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<Router>
+			<Header />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/user' element={<User />} />
+				<Route path='/signin' element={<Signin />} />
+				<Route path='*' element={<Error />} />
+			</Routes>
+			<Footer />
+		</Router>
+	</React.StrictMode>
+);
