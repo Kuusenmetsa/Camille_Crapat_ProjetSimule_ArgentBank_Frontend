8dc8b03b9070dbc17c4201c391b261cc71cceaba
Camille CRAPAT <camille.capt@gmail.com>
2024-07-29 14:45:52 +0200

    Ajout d'import

--- src/utils/Hook/sessionManagement.jsx
+++ src/utils/Hook/sessionManagement.jsx
@@ -1,5 +1,4 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { addConnection } from '../Store/store';
+import { useSelector } from 'react-redux';
 
 export function useTestConnect() {
 	if (useSelector((state) => state.Auth.token) !== null) {
@@ -8,16 +7,5 @@ export function useTestConnect() {
 		return 1;
 	} else {
 		return 0;
-	}
-}
-
-export function useTokenUpdate() {
-	const dispatch = useDispatch();
-	const tokenStore = useSelector((state) => state.Auth.token);
-	if (!tokenStore) {
-		if (localStorage.getItem('token') || sessionStorage.getItem('token')) {
-			const token = localStorage.getItem('token') ? localStorage.getItem('token') : sessionStorage.getItem('token');
-			dispatch(addConnection({ token: token }));
-		}
 	}
 }
